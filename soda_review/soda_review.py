965974bc5b3a695ec3d673632b1ba7e25a564d2d
tbumgard <tbumgard@gmail.com>
2023-10-05 12:45:23 -0400

    Added setter functions for User class

--- soda_review/soda_review.py
+++ soda_review/soda_review.py
@@ -23,12 +23,13 @@ class User:
         self.__picture = picture
         self.__first_name = first_name
         self.__last_name = last_name
-        self.__date_joined = None
+        self.__date_joined = time.asctime()
         self.__reviews = []
         self.__number_of_reviews = 0
     
     def __repr__(self):
-        return f"\nUsername: {self.__user_name}\nPassword: {self.__password}\nEmail: {self.__email}\nFirst Name: {self.__first_name}\nLast Name: {self.__last_name}\n"
+        return (f"\nUsername: {self.__user_name}\nPassword: {self.__password}\nEmail: {self.__email}\n" + 
+                f"First Name: {self.__first_name}\nLast Name: {self.__last_name}\nDate Joined: {self.__date_joined}\n")
     
     # Start of getter functions
     def get_user_name(self):
@@ -59,6 +60,33 @@ class User:
         return self.__number_of_reviews
     # End of getter functions
 
+    # Start of setter functions
+    def set_user_name(self, user_name):
+        self.__user_name = user_name
+        return self.__user_name
+    
+    def set_password(self, password):
+        self.__password = password
+        return self.__password
+    
+    def set_email(self, email):
+        self.__email = email
+        return self.__email
+    
+    def set_picture(self, picture):
+        self.__picture = picture
+        return self.__picture
+    
+    def set_first_name(self, first_name):
+        self.__first_name = first_name
+        return self.__first_name
+    
+    def set_last_name(self, last_name):
+        self.__last_name = last_name
+        return self.__last_name
+    # End of setter functions
+    
+
 # Soda Class
     #name
     #company
@@ -80,6 +108,7 @@ def main():
 
     user1 = User('tbumgard', 'qwerty123', 'tbumgard@gmail.com', first_name='Trevor', last_name='Bumgardner')
     print(user1)
+    
 
 main()
 
