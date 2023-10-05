90b9c7de9b5bb53759506fd08fd9455b8ff2c4ac
tbumgard <tbumgard@gmail.com>
2023-10-05 11:27:19 -0400

    Created soda_review.py with initial project road map

---
+++ soda_review.py
@@ -0,0 +1,33 @@
+# Backend for Soda Review
+
+# Load master list for Sodas from database or file(testing locally- while developing)
+# Load master list for Users from database or file (testing locally- while developing)
+# Load master list for Reviews from database or file (testing locally- while developing)
+
+# User Class
+    #username
+    #password
+    #picture - optional
+    #first name - optional
+    #last name - optional
+    #join data
+    #email
+    #list of submitted reviews
+    #number of reviews done
+
+# Soda Class
+    #name
+    #company
+    #picture
+    #list of reviews
+    #average review rating
+    #average weighted review rating
+
+# Review Class
+    #Soda
+    #User 
+    #Review
+    #Rating
+    #Up votes
+    #Down votes
+
